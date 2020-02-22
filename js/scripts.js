491e49f2fc1abf24fc36c6a2760eee354c3e16ce
jadielmwangi <jedielmwangi@gmail.com>
2020-02-22 21:58:00 +0300

    adds function in  jQuery.js for toggle

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+   $(".what img") .click(function(){
+       $("#first p").toggle();
+       $()
+
+   });
+});
+
+
+$(document).ready(function(){
+$("form").submit(function(event){
+    var name =$("#name").value();
+    alert("Hey" + name + ",Thanks for reaching to us.We have recieved your message .");
+});
+
+
+});
