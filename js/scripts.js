a72fa830c4ed4c45a9fe76c0db05131ce18f5a69
jadielmwangi <jedielmwangi@gmail.com>
2020-02-23 17:20:12 +0300

    adds jQeury function to enhance toggles

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,41 @@
+
+
+
+
 $(document).ready(function(){
-   $(".what img") .click(function(){
-       $("#first p").toggle();
-       $()
+   $(".image1") .click(function(){
+       $("#first").toggle();
+       $("#second").toggle();
+       $("#third").toggle();
 
    });
 });
 
 
+
+
+
+// $("#develop") .click(function(){
+//     $("image2") .toggle();
+// });
+// $("#image") .click(function(){
+//     $("second") .toggle();
+// });
+// });
+
+// $(document).ready(function(){
+// $("form").submit(function(event){
+//     var name =$("#name").value();
+//     alert("Hey" + name + ",Thanks for reaching to us.We have recieved your message .");
+// });
+
+
+// });
+
 $(document).ready(function(){
-$("form").submit(function(event){
-    var name =$("#name").value();
-    alert("Hey" + name + ",Thanks for reaching to us.We have recieved your message .");
-});
-
-
-});
+    $("image").hover(function(){
+      $(this).css("background-color", "black");
+      }, function(){
+      $(this).css("background-color", "pink");
+    });
+  });
