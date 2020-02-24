1eeb666a4e996041551c98aef04913b933bde7dc
jadielmwangi <jedielmwangi@gmail.com>
2020-02-24 13:16:16 +0300

    adds js function that get input from the form

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,68 @@
 
-
-
-$(document).ready(function(){
-   $(".image1") .click(function(){
-       $("#first").toggle();
-
-   });
-   $(".image2") .click(function(){
-    $("#second").toggle();
-   });
-   $(".image3") .click(function(){
-    $("#third").toggle();
-   });
-   
-
-$("#w4") .hover(function(){
-$("#port4") .toggle()
-
-});
-$("#w3") .hover(function(){
-  $("#port3") .toggle();
-  relative
+$(document).ready(function () {
+  $(".image1").click(function () {
+    $("#first").slideToggle();
   });
-  $("#w2") .hover(function(){
-    $("#port2") .toggle();
-    
+
+    $(".image2").click(function () {
+      $("#second").slideToggle();
+  
     });
-    $("#w1") .hover(function(){
-      $("#port1") .toggle();
-      
+      $(".image3").click(function () {
+        $("#third").slideToggle();
+  
       });
 
-    $("#w5") .hover(function(){
-        $("#port5") .toggle();
-        
-        });
-     $("#w6") .hover(function(){
-      $("#port6") .toggle();
-          
-          });
-      $("#w7") .hover(function(){
-      $("#port7") .toggle();
-            
-            });
-      $("#w8") .hover(function(){
-      $("#port8") .toggle();
-              
-              });
+
+  $("#w4").hover(function () {
+    $("#port4").toggle()
+
+  });
+  $("#w3").hover(function () {
+    $("#port3").toggle();
+  });
+  $("#w2").hover(function () {
+    $("#port2").toggle();
+
+  });
+  $("#w1").hover(function () {
+    $("#port1").toggle();
+
+  });
+
+  $("#w5").hover(function () {
+    $("#port5").toggle();
+
+  });
+  $("#w6").hover(function () {
+    $("#port6").toggle();
+
+  });
+  $("#w7").hover(function () {
+    $("#port7").toggle();
+
+  });
+  $("#w8").hover(function () {
+    $("#port8").toggle();
+
+  });
 });
 
 
 
 
-$(document).ready(function(){
-$("form").submit(function(event){
-    var name =$("#name").value();
-    alert("Hey" + name + ",Thanks for reaching to us.We have recieved your message .");
-});
 
 
-});
+
+
+
+
+
+ function nameOfUser(submit){
+  
+    var name = $("input#name").val();
+    var email =$("input#email").val();
+
+    alert("Hey " + name + ",Thanks for reaching to us.We have recieved your message .");
+    event.preventDefault();
+  };
