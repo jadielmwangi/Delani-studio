108e0af759c07210f13fe649efdb40bf63e975b3
jadielmwangi <jedielmwangi@gmail.com>
2020-02-24 08:34:16 +0300

    adds hover(function) to ensure there is hovering

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,40 @@ $(document).ready(function(){
     $("#third").toggle();
    });
    
+
+$("#w4") .hover(function(){
+$("#port4") .toggle();
+
+});
+$("#w3") .hover(function(){
+  $("#port3") .toggle();
+  
+  });
+  $("#w2") .hover(function(){
+    $("#port2") .toggle();
+    
+    });
+    $("#w1") .hover(function(){
+      $("#port1") .toggle();
+      
+      });
+
+    $("#w5") .hover(function(){
+        $("#port5") .toggle();
+        
+        });
+     $("#w6") .hover(function(){
+      $("#port6") .toggle();
+          
+          });
+      $("#w7") .hover(function(){
+      $("#port7") .toggle();
+            
+            });
+      $("#w8") .hover(function(){
+      $("#port8") .toggle();
+              
+              });
 });
 
 
